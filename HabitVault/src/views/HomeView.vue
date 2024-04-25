c05bf77798a40299d5c3f1f88b6e9bdcc4139c11
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-04-25 20:44:50 +0200

    Add `Vue Router` and remove unnecessary components and assets

---
+++ HabitVault/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="home">
+        <h1>Home page</h1>
+    </div>
+</template>
+
+<style>
+.home {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+</style>
