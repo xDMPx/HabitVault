5d2826c3b98d860a7464a31942fbfe0510fb3ff8
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-04-26 19:53:09 +0200

    Backend: Implement basic login/auth functionality and session management

--- HabitVault-backend/app.ts
+++ HabitVault-backend/app.ts
@@ -1,12 +1,29 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import session from 'express-session'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'testSecret',
+    name: "session",
+    cookie: {
+        maxAge: 600000,
+        secure: false,
+        sameSite: 'lax'
+    }
+}))
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 const port = 3000
 
-app.get('/users', async (req: Request, res: Response) => {
+app.get('/users', restrict, async (req: Request, res: Response) => {
     console.log(`${req.method} ${req.url}`)
 
     const users = await prisma.user.findMany()
@@ -18,6 +35,7 @@ interface RegisterBody {
     password: string | undefined
 
 }
+
 interface TypedRequest<T> extends Request {
     body: T
 }
@@ -45,6 +63,53 @@ app.post('/register', async (req: TypedRequest<RegisterBody>, res: Response) => 
 
 })
 
+declare module 'express-session' {
+    interface SessionData {
+        userid: number | undefined
+    }
+}
+
+interface LoginBody {
+    username: string | undefined,
+    password: string | undefined
+}
+
+app.post('/login', async (req: TypedRequest<LoginBody>, res: Response) => {
+    console.log(`${req.method} ${req.path} => ${JSON.stringify(req.body)}`)
+
+    const username = req.body.username
+    const password = req.body.password
+    if (username !== undefined && password !== undefined) {
+        const user = await prisma.user.findFirst({
+            where: {
+                username: username,
+            }
+        })
+        if (user?.password === password) {
+            console.log("loged in")
+            req.session.regenerate(function () {
+                console.log(`loged in regenerat ${req.session.id}`)
+                req.session.userid = user.id
+                res.json()
+            });
+        }
+        else {
+            res.status(401).json()
+        }
+
+    } else {
+        res.status(400).json()
+    }
+
+})
+
+function restrict(req: Request, res: Response, next: NextFunction) {
+    if (req.session.userid !== undefined) {
+        next()
+    } else {
+        res.status(401).json()
+    }
+}
 
 app.listen(port, () => {
     console.log(`HabitVault backend listening on port ${port}`)
