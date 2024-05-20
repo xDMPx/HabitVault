048e3d1053d6583e6fa2bd733e48a2a144242247
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-05-20 11:23:57 +0200

    Frontend: Refactor authorization state management in `App.vue`. Utilize enum for state management.

--- HabitVault/src/App.vue
+++ HabitVault/src/App.vue
@@ -3,45 +3,51 @@ import UnauthorizedView from './views/UnauthorizedView.vue'
 import AuthorizedView from './views/AuthorizedView.vue'
 
 import axios from 'axios'
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
 import { useRouter } from 'vue-router'
 import AdminView from './views/AdminView.vue'
 
 const router = useRouter()
 
-const authorized = ref(false)
+const enum UserType {
+    Unknown,
+    Normal,
+    Admin
+}
+
+const userType: Ref<UserType> = ref(UserType.Unknown)
+
 axios.get('/authorized').then(() => {
-    authorized.value = true
+    userType.value = UserType.Normal
 }).catch(() => { })
 
-const admin = ref(false)
 axios.get('/adminAuthorized').then(() => {
-    authorized.value = false
-    admin.value = true
+    userType.value = UserType.Admin
     router.push('/admin')
-}).catch(() => { admin.value = false })
+})
 
 function updateAuthState(auth: boolean) {
-    authorized.value = auth
-    admin.value = false
-    if (auth !== false) {
+    if (auth) {
         axios.get('/adminAuthorized').then(() => {
-            authorized.value = false
-            admin.value = true
+            userType.value = UserType.Admin
             router.push('/admin')
-        }).catch(() => { admin.value = false })
+        }).catch(() => { userType.value = UserType.Normal })
+    }
+    else if (auth === false) {
+        userType.value = UserType.Unknown
     }
 }
+
 </script>
 
 <template>
-    <div class="bg-base-300" v-if="admin">
+    <div class="bg-base-300" v-if="userType === UserType.Admin">
         <AdminView @updateAuthState="updateAuthState" />
     </div>
-    <div class="bg-base-300" v-else-if="!authorized">
+    <div class="bg-base-300" v-else-if="userType === UserType.Unknown">
         <UnauthorizedView @updateAuthState="updateAuthState" />
     </div>
-    <div class="bg-base-300" v-else="!admin">
+    <div class="bg-base-300" v-else>
         <AuthorizedView @updateAuthState="updateAuthState" />
     </div>
 </template>
