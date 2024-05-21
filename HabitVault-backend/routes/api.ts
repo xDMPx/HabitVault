0b88f0851ffaf6237d92e7696f11eb01c8fe4547
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-05-21 17:20:18 +0200

    Backend: Implement password hashing with the help of `Argon2` package

--- HabitVault-backend/routes/api.ts
+++ HabitVault-backend/routes/api.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
+import * as argon2 from "argon2"
 
 import { restrict, adminRestrict } from '../middlewares'
 import { TypedRequest, RegisterBody, LoginBody } from '../interfaces'
 
 const router = Router()
 const prisma = new PrismaClient()
+
 
 router.get('/authorized', restrict, async (_req: Request, res: Response) => {
     res.json()
@@ -49,14 +51,17 @@ router.post('/register', async (req: TypedRequest<RegisterBody>, res: Response) 
         return
     }
 
-    const user = await prisma.user.create({
-        data: {
-            username: username,
-            password: password
-        }
+    argon2.hash(password).then(async (passhash) => {
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password: passhash
+            }
+        })
+        res.json(user)
+    }).catch((_err) => {
+        res.status(500).json()
     })
-    res.json(user)
-
 })
 
 function isValidUserName(username: string): Boolean {
@@ -71,26 +76,43 @@ router.post('/login', async (req: TypedRequest<LoginBody>, res: Response) => {
         const user = await prisma.user.findFirst({
             where: {
                 username: username,
+            },
+            select: {
+                username: true,
+                password: true
             }
         })
-        if (user?.password === password) {
-            req.session.regenerate(() => {
-                req.session.username = user.username
-                res.json()
-            });
-        }
-        else {
-            res.status(401).json()
+        if (user?.password !== undefined) {
+            argon2.verify(user?.password, password).then((result) => {
+                if (result) {
+                    req.session.regenerate(() => {
+                        req.session.username = user.username
+                        res.json()
+                    })
+                } else {
+                    res.status(400).json()
+                }
+            }
+            ).catch((_err) => {
+                console.log(_err)
+                res.status(500).json()
+            })
+        } else {
+            res.status(400).json()
         }
     } else {
         res.status(400).json()
     }
 })
 
-//TODO Handle error
 router.post('/signout', restrict, async (req: Request, res: Response) => {
     req.session.destroy((_err) => {
-        res.json()
+        if (_err === undefined) {
+            res.json()
+        } else {
+            console.log(_err)
+            res.status(500).json()
+        }
     })
 })
 
