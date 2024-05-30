a457763109b43a5393bf9b8a52ad246019ba45a9
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-05-30 22:02:10 +0200

    Backend: Prevent deletion of last admin account via delete user endpoint

--- HabitVault-backend/routes/api/admin.ts
+++ HabitVault-backend/routes/api/admin.ts
@@ -24,7 +24,6 @@ router.get('/users', adminRestrict, async (_req: Request, res: Response, next: N
     }
 })
 
-//TODO: ensure that at least one admin account exists
 router.delete('/user/:username', adminRestrict, async (req: TypedRequest<any, { username: string }>, res: Response, next: NextFunction) => {
     try {
         const username = req.params.username
@@ -34,6 +33,21 @@ router.delete('/user/:username', adminRestrict, async (req: TypedRequest<any, { 
                 error: "Invalid Username"
             })
             return
+        }
+
+        const user = await prisma.user.findFirst({
+            where: { username: username },
+        })
+        if (user?.admin) {
+            const admin_count = await prisma.user.count({
+                where: { admin: true }
+            })
+            if (admin_count - 1 <= 0) {
+                res.status(400).json({
+                    error: "At least one admin account must exist."
+                })
+                return
+            }
         }
 
         if (username !== undefined) {
