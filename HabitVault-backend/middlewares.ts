69ede85ebcfa0ddee67ec35df4fced6985442a2c
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-06-03 10:08:41 +0200

    Backend: Refactor JWT Middleware, reduce duplication

--- HabitVault-backend/middlewares.ts
+++ HabitVault-backend/middlewares.ts
@@ -27,37 +27,19 @@ export function restrict(req: Request, res: Response, next: NextFunction) {
 
 export async function restrictJWT(req: Request, res: Response, next: NextFunction) {
     try {
-
-        let token = req.cookies.authToken as string || undefined
-
-        if (token === undefined) {
-            const authHeader = req.headers['authorization']
-            if (authHeader === undefined || !authHeader.startsWith('Bearer ')) {
-                restrict(req, res, next)
-                return
-            } else {
-                token = authHeader.split(' ')[1]
-            }
+        const token = getJWT(req)
+        if (token === null) {
+            restrict(req, res, next)
+            return
         }
 
-        const decoded = jwt.verify(token, "testKey")
-        if (typeof (decoded) !== 'object') {
+        const username = decodeJWT(token)
+        if (username === null) {
             res.status(401).json()
             return
         }
 
-        const username = decoded.username
-        const lastID = +(await redis.get(`${username}:lastID`) ?? 'NaN')
-        let validToken = true
-        if (!isNaN(lastID)) {
-            for (let i = 0; i <= lastID; i++) {
-                const blacklistedToken = await redis.get(`${username}:${i}`)
-                if (blacklistedToken === token) {
-                    validToken = false
-                    break
-                }
-            }
-        }
+        const validToken = await isValidToken(token, username)
 
         if (validToken) {
             res.locals.username = username
@@ -95,37 +77,19 @@ export async function adminRestrict(req: Request, res: Response, next: NextFunct
 
 export async function adminRestrictJWT(req: Request, res: Response, next: NextFunction) {
     try {
-
-        let token = req.cookies.authToken as string || undefined
-
-        if (token === undefined) {
-            const authHeader = req.headers['authorization']
-            if (authHeader === undefined || !authHeader.startsWith('Bearer ')) {
-                adminRestrict(req, res, next)
-                return
-            } else {
-                token = authHeader.split(' ')[1]
-            }
+        const token = getJWT(req)
+        if (token === null) {
+            adminRestrict(req, res, next)
+            return
         }
 
-        const decoded = jwt.verify(token, "testKey")
-        if (typeof (decoded) !== 'object') {
+        const username = decodeJWT(token)
+        if (username === null) {
             res.status(401).json()
             return
         }
 
-        const username = decoded.username
-        const lastID = +(await redis.get(`${username}:lastID`) ?? 'NaN')
-        let validToken = true
-        if (!isNaN(lastID)) {
-            for (let i = 0; i <= lastID; i++) {
-                const blacklistedToken = await redis.get(`${username}:${i}`)
-                if (blacklistedToken === token) {
-                    validToken = false
-                    break
-                }
-            }
-        }
+        const validToken = await isValidToken(token, username)
 
         if (validToken) {
             const admin = await prisma.user.findFirst({
@@ -151,4 +115,41 @@ export async function adminRestrictJWT(req: Request, res: Response, next: NextFu
     }
 }
 
+function getJWT(req: Request): string | null {
+    let token = req.cookies.authToken as string || undefined
 
+    if (token === undefined) {
+        const authHeader = req.headers['authorization']
+        if (authHeader === undefined || !authHeader.startsWith('Bearer ')) {
+            return null
+        } else {
+            token = authHeader.split(' ')[1]
+        }
+    }
+    return token
+}
+
+function decodeJWT(token: string): string | null {
+    const decoded = jwt.verify(token, "testKey")
+    if (typeof (decoded) !== 'object') {
+        return null
+    }
+
+    const username = decoded.username
+    return username
+}
+
+async function isValidToken(token: string, username: string): Promise<boolean> {
+    const lastID = +(await redis.get(`${username}:lastID`) ?? 'NaN')
+    let validToken = true
+    if (!isNaN(lastID)) {
+        for (let i = 0; i <= lastID; i++) {
+            const blacklistedToken = await redis.get(`${username}:${i}`)
+            if (blacklistedToken === token) {
+                validToken = false
+                break
+            }
+        }
+    }
+    return validToken
+}
